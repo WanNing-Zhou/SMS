f64944018d1fb38a02ebf4243d6a00d8bd5a9578
WangNing-Zhou <11344283+wangning-zhou@user.noreply.gitee.com>
2022-10-16 12:28:35 +0800

    down Element-ui and axios

--- smsp/vue.config.js
+++ smsp/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    port: 9527, //端口号
+    host: "localhost", //主机名,127.0.0.1
+    https: false, //协议
+    open: true //启动服务器时自动打开浏览器
+  },
+  lintOnSave: false, //关闭格式检查
   transpileDependencies: true
 })
